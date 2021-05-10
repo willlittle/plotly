4003393f4e50b0af51cd3771537b3c4a5e9fff6f
Spaceforce <spaceforce.dev@gmail.com>
2021-05-10 19:36:49 +1000

    work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import {useState} from 'react';
 import { Tabs, Tab, AppBar } from '@material-ui/core';
-import Chart from './Chart'
+import Plot from 'react-plotly.js';
 import * as d3 from 'd3'
+import chart from './Chart'
 
 const iState = () => {
   let year = []
@@ -18,31 +19,34 @@ const iState = () => {
     return initialState
 }
 
-function dataSwitcher(tabIndex){
-  let year = []
-  let mortality = []
-  let plot = []
-    d3.csv('/mortality2.csv', (d)=> {
-      if(d.index == tabIndex){
-        year.push(d.year)
-        mortality.push(d.recordedRate) 
-        plot.push(d.country)
-      }})
-  const newPlot = {year, mortality, plot}
-  return newPlot
 
-}  
 function App() {
 
 
   const[currentData, setCurrentData] = useState(iState());
   const[tabValue, setTabValue]= useState(0)
-  const handleTabs=(e, val)=>(setTabValue(val), dataSwitcher(val))
-
-  const updateData = (tabIndex) =>{
-    setCurrentData(dataSwitcher(tabIndex))
-
+  const handleTabs=(e, val) => {
+    setTabValue(val);
+    dataSwitcher(val);
   }
+
+  async function dataSwitcher (tabIndex){
+    let year = []
+    let mortality = []
+    let plot = []
+      await d3.csv('/mortality2.csv', (d)=> {
+        if(d.index == tabIndex){
+          year.push(d.year)
+          mortality.push(d.recordedRate) 
+          plot.push(d.country)
+        }})
+      
+    const newPlot = {year, mortality, plot}
+    console.log(newPlot);
+    setCurrentData(newPlot)
+    return newPlot
+  }  
+
 
 // console.log(currentData.mortality)
     return (
@@ -50,7 +54,7 @@ function App() {
       <div className='container'>
           <div>
             <AppBar position='static' >
-              <Tabs centered align={'center'}value={tabValue} onChange={handleTabs}>
+              <Tabs centered align={'center'} value={tabValue} onChange={handleTabs}>
                 <Tab label='Plot 1'/>
                 <Tab label='Plot 2'/>
                 <Tab label='Plot 3'/>
@@ -58,7 +62,15 @@ function App() {
             </AppBar>
           </div>
           <div>
-            <Chart plotData={currentData} />
+          {/* <Plot 
+            data={[
+            {type:'bar',
+            x:currentData.year,
+            y:currentData.mortality}
+            ]} 
+            layout={ {autosize: true, paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0, 0, 0, 0)'}}
+        />  */}
+              {/* <Chart plotData={currentData}/> */}
           </div>
         </div> 
     </div>
--- src/Chart.js
+++ src/Chart.js
@@ -2,42 +2,52 @@ import React, {Component} from 'react';
 import Plot from 'react-plotly.js';
 import './App.css'
 
-class Chart extends Component {
-    // constructor(props){
-    //     super()
-    //     this.state = {
-    //     year: [],
-    //     mortality: []
 
-    // }}
-    
- 
-    render(){
-        var year = this.props.plotData.year
-        var mortality = this.props.plotData.mortality
-
-        var display = false
-    console.log(this.props.plotData.mortality)
-        // typeof this.props.plotData.year[0]=='undefined' ? display = false : display=true
-        true ? display = true : display=false
-        const renderContent = () => {
-            if (display){
-            return <Plot 
-                    data={[
-                        {type:'bar',
-                        x:year,
-                        y:mortality}
-                        ]} 
-                        layout={ {autosize: true, paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0, 0, 0, 0)'}}
-                    /> 
-            } else {
-                return <h1>Please select country </h1>
-            }}
-        return(
-            <div >
-                {renderContent()}
-            </div>
-        )
-    }
+function Chart (plotData) {
+    var year = plotData.year
+    var mortality = plotData.mortality
+    return <Plot 
+        data={[
+        {type:'bar',
+        x:year,
+        y:mortality}
+        ]} 
+        layout={ {autosize: true, paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0, 0, 0, 0)'}}
+    /> 
 }
 export default Chart;
+
+
+
+
+// class Chart extends Component {
+ 
+//     render(){
+//         var year = this.props.plotData.year
+//         var mortality = this.props.plotData.mortality
+
+//         var display = false
+//         console.log(this.props.plotData.mortality)
+//         // typeof this.props.plotData.year[0]=='undefined' ? display = false : display=true
+//         true ? display = true : display=false
+//         const renderContent = () => {
+//             if (display){
+//             return <Plot 
+//                     data={[
+//                         {type:'bar',
+//                         x:year,
+//                         y:mortality}
+//                         ]} 
+//                         layout={ {autosize: true, paper_bgcolor: 'rgba(0,0,0,0)', plot_bgcolor: 'rgba(0, 0, 0, 0)'}}
+//                     /> 
+//             } else {
+//                 return <h1>Please select country </h1>
+//             }}
+//         return(
+//             <div >
+//                 {renderContent()}
+//             </div>
+//         )
+//     }
+// }
+// export default Chart;
